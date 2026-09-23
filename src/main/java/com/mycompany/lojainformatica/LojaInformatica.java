0a24a07bf3436b65c7b899c1197c081adda1cd87
Mateus Michelon Moras <mateusm.moras@universo.univates.br>
2026-09-22 21:11:01 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/lojainformatica/LojaInformatica.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lojainformatica;
+
+/**
+ *
+ * @author mateus.moras
+ */
+public class LojaInformatica {
+
+    public static void main(String[] args) {
+        TelaMenu tm = new TelaMenu();
+        tm.setVisible(true);
+    }
+}
